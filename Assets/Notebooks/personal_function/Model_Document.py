87f51f66bcffd8a4a597c2918ce0d35a4cd2ad08
devscheffer <devscheffer@gmail.com>
2020-08-23 00:38:33 -0300

    end

--- Assets/Notebooks/personal_function/Model_Document.py
+++ Assets/Notebooks/personal_function/Model_Document.py
@@ -4,27 +4,35 @@ class cls_Model_Document:
 
     def doc_create(self):
         dict_doc = {
-        'model': []
-        ,'accuracy': []
-        ,'method': []
-        ,'classifier': []
-        ,'description': []
+        'param': {
+            'id' : []
+            ,'model': []
+            ,'accuracy': []
+            ,'method': []
+            ,'classifier': []
+            ,'description': []
+            }
         }
         self.dict_doc = dict_doc
 
+
+    def doc_set_id(self,text):
+        # self.dict_doc['param']['id'].append(text)
+        self.dict_doc['param']['id'] = text
+
+
     def doc_add_method(self,text):
-        self.dict_doc['method'].append(text)
+        self.dict_doc['param']['method'].append(text)
 
     def doc_add_model(self,text):
-        self.dict_doc['model'].append(text)
+        self.dict_doc['param']['model'].append(text)
 
     def doc_add_classifier(self,text):
-        self.dict_doc['classifier'].append(text)
+        self.dict_doc['param']['classifier'].append(text)
 
     def doc_add_description(self,text):
-        self.dict_doc['description'].append(text)
+        self.dict_doc['param']['description'].append(text)
 
     def doc_add_accuracy(self,text):
-        self.dict_doc['accuracy'].append(text)
-
+        self.dict_doc['param']['accuracy'].append(text)
 
